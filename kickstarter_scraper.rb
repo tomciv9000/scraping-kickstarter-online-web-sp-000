3184b6c9fb8d87434c8b4221053dbf31ab66464e
Tom White <eugene.white@gmail.com>
2019-06-22 01:14:15 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,6 +11,9 @@ end
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
+#Tom's description attempt: project.css("p.bbcard_blurb").#text.delete("\n")#####
+
+
 
 
 create_project_hash
