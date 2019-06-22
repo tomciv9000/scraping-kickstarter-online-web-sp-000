37c1bec41233c6652a8e8ed4748f75e93f84cea0
Tom White <eugene.white@gmail.com>
2019-06-22 01:40:06 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,8 +11,9 @@ end
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
-# description: project.css("p.bbcard_blurb").####text.delete("\n")#####
+# description: project.css("p.bbcard_blurb").text   ###.delete("\n")#####
 # location: project.css("ul.project-meta span.location-name").text
+# percent funded: project.css("ul.project-stats li.first.funded strong").text
 
 
 
